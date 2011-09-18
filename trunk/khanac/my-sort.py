f125440c0b14ad7222a9152bb1fa6b9e8f133162
ndjapic@gmail.com <ndjapic@gmail.com@f3fcb5c9-a8c4-87ab-5a5d-a170bb4eb25d>
2011-09-18 09:49:48 +0000

    for sintax repaired

--- trunk/khanac/my-sort.py
+++ trunk/khanac/my-sort.py
@@ -1,25 +1,25 @@
 def my-sort(a):
   LEFT = 0; HEAD = 1; RIGHT = 2
-  
+
   def new-tree(): return []
-  def not-empty(t): return t.length == 3
-  
+  def is-empty(t): return t.length < 3 # or < 1, whatever
+
   def put-value-to-tree(v,t):
-    if(not-empty(t)):
-      if(v < t[HEAD]): put-value-to-tree(v,t[LEFT])
-      else:            put-value-to-tree(v,t[RIGHT])
-    else:
+    if(is-empty(t)):
       t.append(new-tree())
       t.append(v)
       t.append(new-tree())
+    elif(v < t[HEAD]): put-value-to-tree(v,t[LEFT])
+    else:              put-value-to-tree(v,t[RIGHT])
 
   def read-tree(t):
-    if(not-empty(t)):
+    if(not is-empty(t)):
       read-tree(t[LEFT])
-      a[i++] = t[HEAD]
+      a[i] = t[HEAD]
+      i += 1
       read-tree(t[RIGHT])
-  
+
   tree = new-tree()
-  for(v in a): put-value-to-tree(v,tree)  
+  for v in a: put-value-to-tree(v,tree)  
   i = 0
   read-tree(tree)
