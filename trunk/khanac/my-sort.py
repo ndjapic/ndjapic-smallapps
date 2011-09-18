3f9688e587b6113029eeaf24cb635f05b1cc27cd
ndjapic@gmail.com <ndjapic@gmail.com@f3fcb5c9-a8c4-87ab-5a5d-a170bb4eb25d>
2011-09-18 09:29:52 +0000

    added not-empty(t)

--- trunk/khanac/my-sort.py
+++ trunk/khanac/my-sort.py
@@ -1,24 +1,25 @@
 def my-sort(a):
   LEFT = 0; HEAD = 1; RIGHT = 2
   
-  def new-node(): return []
+  def new-tree(): return []
+  def not-empty(t): return t.length == 3
   
   def put-value-to-tree(v,t):
-    if(t.length == 3):
+    if(not-empty(t)):
       if(v < t[HEAD]): put-value-to-tree(v,t[LEFT])
       else:            put-value-to-tree(v,t[RIGHT])
     else:
-      t.append(new-node())
+      t.append(new-tree())
       t.append(v)
-      t.append(new-node())
+      t.append(new-tree())
 
   def read-tree(t):
-    if(t.length == 3):
+    if(not-empty(t)):
       read-tree(t[LEFT])
       a[i++] = t[HEAD]
       read-tree(t[RIGHT])
   
-  tree = new-node()
+  tree = new-tree()
   for(v in a): put-value-to-tree(v,tree)  
   i = 0
   read-tree(tree)
