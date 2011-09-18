38cdf4bc52bd8c383e896be28fb09f0864b440c6
ndjapic@gmail.com <ndjapic@gmail.com@f3fcb5c9-a8c4-87ab-5a5d-a170bb4eb25d>
2011-09-18 09:01:00 +0000

    Maybe works because arrays are objects?

--- trunk/khanac/my-sort.py
+++ trunk/khanac/my-sort.py
@@ -1,16 +1,24 @@
 def my-sort(a):
-  LEFT = 0
-  HEAD = 1
-  RIGHT = 2
-  tree = []
-  for(x in a):
-    parent = []
-    node = tree
-    while(node.length == 3):
-      parent = node
-      if(x < parent[HEAD]):
-        node = parent[LEFT]
-      else:
-        node = parent[RIGHT]
-    node = [ [], x, [] ]
-# this will not work
+  LEFT = 0; HEAD = 1; RIGHT = 2
+  
+  def new-node(): return []
+  
+  def put-value-to-tree(v,t):
+    if(t.length == 3):
+      if(v < t[HEAD]): put-value-to-tree(v,t[LEFT])
+      else:            put-value-to-tree(v,t[RIGHT])
+    else:
+      t.append(new-node())
+      t.append(v)
+      t.append(new-node())
+
+  def read-tree(t):
+    if(t.length == 3):
+      read-tree(t[LEFT])
+      a[i++] = t[HEAD]
+      read-tree(t[RIGHT])
+  
+  tree = newnode()
+  for(v in a): put-value-to-tree(v,tree)  
+  i = 0
+  read-tree(tree)
