a516b1951b7bbfd62aea2825e61665604f88c895
ndjapic@gmail.com <ndjapic@gmail.com@f3fcb5c9-a8c4-87ab-5a5d-a170bb4eb25d>
2011-09-25 14:30:15 +0000

    hyphen dash in identifiers changed into underscore sign

--- trunk/khanac/my-sort.py
+++ trunk/khanac/my-sort.py
@@ -1,25 +1,25 @@
-def my-sort(a):
+def my_sort(a):
   LEFT = 0; HEAD = 1; RIGHT = 2
 
-  def new-tree(): return []
-  def is-empty(t): return len(t) < 3 # or < 1, whatever
+  def new_tree(): return []
+  def is_empty(t): return len(t) < 3 # or < 1, whatever
 
-  def put-value-to-tree(v,t):
-    if(is-empty(t)):
-      t.append(new-tree())
+  def put_value_to_tree(v,t):
+    if(is_empty(t)):
+      t.append(new_tree())
       t.append(v)
-      t.append(new-tree())
-    elif(v < t[HEAD]): put-value-to-tree(v,t[LEFT])
-    else:              put-value-to-tree(v,t[RIGHT])
+      t.append(new_tree())
+    elif(v < t[HEAD]): put_value_to_tree(v,t[LEFT])
+    else:              put_value_to_tree(v,t[RIGHT])
 
-  def read-tree(t):
-    if(not is-empty(t)):
-      read-tree(t[LEFT])
+  def read_tree(t):
+    if(not is_empty(t)):
+      read_tree(t[LEFT])
       a[i] = t[HEAD]
       i += 1
-      read-tree(t[RIGHT])
+      read_tree(t[RIGHT])
 
-  tree = new-tree()
-  for v in a: put-value-to-tree(v,tree)  
+  tree = new_tree()
+  for v in a: put_value_to_tree(v,tree)  
   i = 0
-  read-tree(tree)
+  read_tree(tree)
