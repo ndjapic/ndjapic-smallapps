1c00f8c5f6608e957da47200236f0e8267b9dbc5
ndjapic@gmail.com <ndjapic@gmail.com@f3fcb5c9-a8c4-87ab-5a5d-a170bb4eb25d>
2011-09-25 18:55:27 +0000

    if syntax changed

--- trunk/khanac/my-sort.py
+++ trunk/khanac/my-sort.py
@@ -1,17 +1,16 @@
 def my_sort(a):
-  LEFT = 0; HEAD = 1; RIGHT = 2
+  LEFT = 0; HEAD = 1; RIGHT = 2; i = 0
 
   def new_tree(): return []
   def is_empty(t): return len(t) < 1
 
   def put_value_to_tree(v,t):
-    if(is_empty(t)):   t += [new_tree(), v, new_tree()]
-    elif(v < t[HEAD]): put_value_to_tree(v,t[LEFT])
-    else:              put_value_to_tree(v,t[RIGHT])
+    if is_empty(t):   t += [new_tree(), v, new_tree()]
+    elif v < t[HEAD]: put_value_to_tree(v,t[LEFT])
+    else:             put_value_to_tree(v,t[RIGHT])
 
-  i = 0
   def read_tree(t):
-    if(not is_empty(t)):
+    if not is_empty(t):
       read_tree(t[LEFT])
       a[i] = t[HEAD] # it says here that local var i is used before inited
       i += 1
