6f28843687efe4d24691a4c6f9dcaf41268804ae
Nenad Đapić <ndjapic@users.noreply.github.com>
2025-03-07 17:08:52 +0100

    Update convert.py

--- datetime/convert.py
+++ datetime/convert.py
@@ -1,7 +1,7 @@
 for i in range(int(input())):
     datum, vreme = input().split('Z')[0].split('T')
     y, m, d = datum.split('-')
-    datum = str(int(d)) + '. ' + str(int(m)) + '. ' + y[2:]
+    datum = '. '.join([str(int(d)), str(int(m)), y[2:], ''])
     h, m, s = vreme.split(':')
-    vreme = str(int(h) + 2) + m
+    vreme = '. '.join([str(int(h) + 2), m])
     print(datum + vreme)
